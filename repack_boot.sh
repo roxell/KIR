33936e28910ec86b507cb709b66cacbbc2bd05e0
Anders Roxell <anders.roxell@linaro.org>
2024-07-11 07:29:20 +0200

    init=/sbin/init
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- repack_boot.sh
+++ repack_boot.sh
@@ -121,7 +121,7 @@ case ${TARGET} in
 				cat "${INITRD_FILE}" > final-initrd.cpio.gz
 				initrd_filename="final-initrd.cpio.gz"
 				cmdline_extra="clk_ignore_unused pd_ignore_unused"
-				cmdline="rootfstype=ext4 noresume rw ${console_cmdline} ${cmdline_extra} -- "
+				cmdline="rootfstype=ext4 init=/sbin/init rw ${console_cmdline} ${cmdline_extra} -- "
 				pagasize=4096
 				;;
 			qrb5165-rb5)
